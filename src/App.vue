82f0574817eb1e80d3251ad5fd523137a9cd9f7c
cs-mac <halfmoonvic@gmail.com>
2019-12-24 14:18:59 +0800

    observe

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    {{ nested.msg }}
+    <div v-if="flag">{{ msg }}</div>
+    <div v-else>{{ msg1 }}</div>
+    <button @click="change">change</button>
+    <button @click="toggle">toggle</button>
   </div>
 </template>
 
@@ -9,9 +12,20 @@ export default {
   name: 'App',
   data() {
     return {
+      flag: true,
+      msg: 'Hello World',
+      msg1: 'Hello Vue',
       nested: {
-        msg: '你好啊，格里高利'
+        nestedMsg: '你好啊，格里高利'
       }
+    }
+  },
+  methods: {
+    change() {
+      this.msg = Math.random()
+    },
+    toggle() {
+      this.flag = !this.flag
     }
   }
 }
