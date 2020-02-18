7dc486b12fc1cbe86a1a060492558b819fdb3ed8
cs-mac <halfmoonvic@gmail.com>
2020-02-18 10:51:40 +0800

    watcher: computed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <div id="app">
-    <!-- <div>{{ msg }}</div> -->
-    <ul>
-      <li v-for="item in list">{{ item }}</li>
-    </ul>
+    <div>{{ name }}</div>
     <button @click="change">change</button>
-    <!-- <button @click="add">add</button> -->
+    <button @click="changeLast">change last name</button>
   </div>
 </template>
 
@@ -13,30 +10,27 @@
 import Vue from 'vue';
 export default {
   name: 'App',
-  // props: {
-  //   title: {
-  //     type: String,
-  //     default: 'test props app'
-  //   }
-  // },
   data() {
     return {
-      // msg: {
-      //   a: 'hello'
-      // },
-      list: [1, 2]
+      firstName: 'wang',
+      lastName: 'lin',
+      useless: 0,
+    }
+  },
+  computed: {
+    name() {
+      if (this.useless > 0) {
+        return `${this.firstName} , ${this.lastName}`
+      }
+      return 'please click change'
     }
   },
   methods: {
     change() {
-      // this.list[1] = 3
-       Vue.set(this.list, 1, 3)
+      this.useless++
     },
-    add() {
-      // this.msg.b = 'world'
-      // Vue.set(this.msg, 'b', 'world')
-      // this.list[2] = 4
-      // this.list.push(4)
+    changeLast() {
+      this.lastName = 'haha'
     }
   }
 }
