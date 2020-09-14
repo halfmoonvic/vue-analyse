8261845240cfb853046209a844ce63aec36673db
cs-mac <halfmoonvic@gmail.com>
2020-09-14 17:50:02 +0800

    router hooks

--- src/main.js
+++ src/main.js
@@ -6,8 +6,43 @@ Vue.use(VueRouter)
 
 // 1. 定义（路由）组件。
 // 可以从其他文件 import 进来
-const Foo = { template: '<div>foo<router-view></router-view></div>' }
-const Bar = { template: '<div>bar</div>' }
+const Foo = {
+  template:
+    '<div>' +
+    '<div>foo</div>' +
+    '<router-link to="bar" :append="true">Go to Bar</router-link>' +
+    '<router-view></router-view>' +
+    '</div>',
+  beforeRouteEnter(to, from, next) {
+    // 在渲染该组件的对应路由被 confirm 前调用
+    // 不能 获取组件实例 this
+    // 因为当守卫执行前，组件实例还没被创建
+    console.log('foo before router enter')
+    console.log('this?', this)
+    next(vm => {
+      console.log('now this?', vm)
+    })
+  },
+  beforeRouteUpdate(to, from, next) {
+    // 在当前路由改变，但是该组件被复用时调用
+    // 举例来说，对于一个带有动态参数的路径 /foo/:id, 在 /foo/1 和 /foo/2 之间跳转的时候，
+    // 由于会渲染同样的 Foo 组件，因此组件实例会被复用。而这个勾子就会在这个情况下被调用
+    // 可以访问组件实例 this
+    console.log('foo before router update')
+    console.log('this?', this)
+    next()
+  },
+}
+
+const Bar = {
+  template: '<div>bar</div>',
+  beforeRouteLeave(to, from, next) {
+    // 导航离开该组件的对应路由时调用
+    // 可以访问组件实例 this
+    console.log('bar before router leave')
+    next()
+  }
+}
 
 // 2. 定义路由
 // 每个路由应该映射一个组件。 其中"component" 可以是
@@ -22,7 +57,11 @@ const routes = [
     children: [
       {
         path: 'bar',
-        component: Bar
+        component: Bar,
+        beforeEnter(to, from, next) {
+          console.log('bar before enter')
+          next()
+        }
       }
     ]
   }
@@ -32,6 +71,20 @@ const routes = [
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
   routes // （缩写）相当于 routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('global before each')
+  next()
+})
+
+router.afterEach((to, from) => {
+  console.log('global after each')
+})
+
+router.beforeResolve((to, from, next) => {
+  console.log('global after resolve')
+  next()
 })
 
 // 4. 创建和挂载根实例。
